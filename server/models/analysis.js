3c342ef3143425c9ecd1819c88688e25e96c67e2
ebemunk <ebemunk@gmail.com>
2015-03-16 00:09:08 -0700

    method for removing analyses from fs

--- server/models/analysis.js
+++ server/models/analysis.js
@@ -1,5 +1,22 @@
 var debug = require('debug')('server:models:analysis');
+
+var fs = require('fs');
+var path = require('path');
+
 var mongoose = require('mongoose');
+var appRoot = require('app-root-path');
 
 var Analysis = mongoose.model('Analysis', require('./analysisSchema.js'));
+
+//want access to the actual remove() method
+Analysis.prototype.remove_base = Analysis.prototype.remove;
+
+//override remove func to also delete the file in disk
+Analysis.prototype.remove = function(callback) {
+	debug('Analysis.prototype.remove');
+
+	fs.unlink(path.join(appRoot.toString(), this.path, this.fileName));
+	this.remove_base();
+};
+
 module.exports = Analysis;
