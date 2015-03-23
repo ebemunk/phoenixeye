8475052d2c97b67f3a3f85d4ed6d8b344a97da11
ebemunk <ebemunk@gmail.com>
2015-03-23 02:16:34 -0700

    split up deps & files task for faster compilation + uglify

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var plumber = require('gulp-plumber');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 
+var uglify = require('gulp-uglify');
+
 var less = require('gulp-less');
 var minifyCSS = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
@@ -11,26 +13,41 @@ var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync');
 
 var files = {
-	js: [
-		'bower_components/angular/angular.min.js',
-		'bower_components/angular-ui-router/release/angular-ui-router.min.js',
-		'client/js/phoenixeye.js',
-		'client/js/*'
-	],
+	js: {
+		dependencies: [
+			'bower_components/angular/angular.min.js',
+			'bower_components/angular-ui-router/release/angular-ui-router.min.js'
+		],
+		files: [
+			'client/js/phoenixeye.js',
+			'client/js/**/*.js'
+		]
+	},
 	less: [
-		'client/less/*'
+		'client/less/*.less'
 	],
 	html: [
 		'client/index.html',
-		'client/templates/*'
+		'client/templates/*.html',
+		'client/partials/*.html'
 	]
 };
 
+gulp.task('js-dependencies', function () {
+	return gulp.src(files.js.dependencies)
+		.pipe(plumber())
+		.pipe(concat('dependencies.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('client/dist'))
+	;
+});
+
 gulp.task('js', function () {
-	return gulp.src(files.js)
+	return gulp.src(files.js.files)
 		.pipe(plumber())
 		.pipe(sourcemaps.init())
 		.pipe(concat('phoenixeye.js'))
+		.pipe(uglify())
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('client/dist'))
 	;
@@ -57,9 +74,9 @@ gulp.task('browser-sync', function () {
 });
 
 gulp.task('watch', ['browser-sync'], function () {
-	gulp.watch(files.js, ['js', browserSync.reload]);
+	gulp.watch(files.js.files, ['js', browserSync.reload]);
 	gulp.watch(files.less, ['less', browserSync.reload]);
 	gulp.watch(files.html).on('change', browserSync.reload);
 });
 
-gulp.task('default', ['js', 'less']);
+gulp.task('default', ['js-dependencies', 'js', 'less']);
