11830049737c47961a091d51fba6910fa655c629
ebemunk <ebemunk@gmail.com>
2015-03-25 02:09:42 -0700

    homepage + controller and file upload

--- client/js/controllers/HomeCtrl.js
+++ client/js/controllers/HomeCtrl.js
@@ -2,8 +2,43 @@
 
 angular.module('phoenixeye')
 .controller('HomeCtrl', [
-	function () {
-		console.log('lol kk ty');
-		this.fafner = 'yea';
+	'$scope',
+	'$upload',
+	'$http',
+	'ngToast',
+	'$state',
+	function HomeCtrl($scope, $upload, $http, ngToast, $state) {
+		this.upload = function(file) {
+			if( file && file.length ) {
+				$upload.upload({
+					url: '/api/images/upload',
+					file: file
+				}).progress(function (event) {
+					console.log('progress', event);
+				}).success(function (data, status) {
+					console.log('success', data, status);
+					var jobId = data.jobId || null;
+					$state.go('image', {
+						image: data.image,
+						jobId: jobId,
+						permalink: data.image.permalink
+					});
+				}).error(function (data, status) {
+					console.log('error', data, status);
+				});
+			}
+		};
+
+		this.submit = function(url) {
+			$http({
+				method: 'POST',
+				url: 'api/images/submit',
+				data: {url: url}
+			}).success(function (data, status) {
+				console.log('success', data, status);
+			}).error(function (data, status) {
+				console.log('error', data, status);
+			});
+		};
 	}
 ]);
