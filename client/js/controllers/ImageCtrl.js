88f81ac45c257491bfaa330b18b9a4855b3e3d87
ebemunk <ebemunk@gmail.com>
2015-03-29 07:36:58 -0700

    image page progress with polling

--- client/js/controllers/ImageCtrl.js
+++ client/js/controllers/ImageCtrl.js
@@ -7,8 +7,46 @@ angular.module('phoenixeye')
 	'ngToast',
 	'$state',
 	'$stateParams',
-	function ImageCtrl($scope, $http, ngToast, $state, $stateParams) {
-		this.par = $stateParams;
-		console.log($stateParams);
+	'$timeout',
+	'PollSvc',
+	function ImageCtrl($scope, $http, ngToast, $state, $stateParams, $timeout, PollSvc) {
+		console.log('stateParams', $stateParams);
+
+		$scope.image = $stateParams.image;
+
+		if( $stateParams.jobId ) {
+			PollSvc.pollUntil({
+				method: 'get',
+				url: 'api/jobs/' + $stateParams.jobId
+			}, function (resp) {
+				return resp.job.status == 'complete';
+			}).promise.then(function (resp) {
+				console.log('job resolved', resp);
+				getAnalyses();
+			});
+		}
+
+		PollSvc.pollUntil({
+			method: 'get',
+			url: 'api/images/' + $stateParams.permalink
+		}, function (resp) {
+			return resp.image.metaComplete;
+		}).promise.then(function (resp) {
+			console.log('image resolved', resp);
+			$scope.image = resp.image;
+			getAnalyses();
+		});
+
+		function getAnalyses() {
+			$http({
+				method: 'get',
+				url: 'api/analyses/' + $scope.image._id
+			}).success(function (resp, status) {
+				console.log('analyses', resp);
+				$scope.analyses = resp.analyses;
+			}).error(function (resp, status) {
+				console.log('error analyses', resp);
+			});
+		}
 	}
 ]);
