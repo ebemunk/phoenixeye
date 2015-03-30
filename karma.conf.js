c62b0c777fb1c435fc8e386dc2ed05cc8b6311e9
ebemunk <ebemunk@gmail.com>
2015-03-29 20:04:24 -0700

    travis stuff

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@
 // Generated on Sun Mar 29 2015 10:00:42 GMT-0700 (Pacific Daylight Time)
 
 module.exports = function(config) {
-  config.set({
+  var configuration = {
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
@@ -75,8 +75,22 @@ module.exports = function(config) {
     browsers: ['Chrome'],
 
 
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+
+
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: true
-  });
+  };
+
+  if(process.env.TRAVIS){
+    configuration.browsers = ['Chrome_travis_ci'];
+  }
+
+  config.set(configuration);
 };
