3902adda5191bd68c2502349c893b46d8d4aefdb
ebemunk <ebemunk@gmail.com>
2015-04-04 00:28:00 -0700

    stop poll on state change

--- client/js/services/PollSvc.js
+++ client/js/services/PollSvc.js
@@ -5,20 +5,39 @@ angular.module('phoenixeye')
 	'$q',
 	'$http',
 	'$timeout',
-	function PollSvc($q, $http, $timeout) {
+	'$rootScope',
+	function PollSvc($q, $http, $timeout, $rootScope) {
 		var defaultTimeout = 3000;
+
+		//list of promises for polls
+		var pollers = [];
+
+		//stop all pollers on state change
+		$rootScope.$on('$stateChangeSuccess', function () {
+			pollers.forEach(function (poller) {
+				poller.stopped = true;
+			});
+		});
 
 		return {
 			pollUntil: function(httpConfig, stopCondition, timeout) {
 				timeout = timeout || defaultTimeout;
 
 				var deferred = $q.defer();
+				pollers.push(deferred);
 
+				//error from server
 				function errorHandler(resp, status) {
 					deferred.reject(resp);
 				}
 
 				function successHandler(resp, status) {
+					//not stopped
+					if( deferred.stopped ) {
+						return deferred.reject();
+					}
+
+					//stop condition not met, try again
 					if( ! stopCondition(resp) ) {
 						$timeout(function () {
 							$http(httpConfig)
