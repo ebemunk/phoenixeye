9c319cd9d54079c40f6c4375892cc4e0c6c677a3
ebemunk <ebemunk@gmail.com>
2015-08-13 21:45:31 -0700

    request analysis test

--- test/src/server/routes/images.js
+++ test/src/server/routes/images.js
@@ -227,4 +227,37 @@ describe('/api/images', function () {
 			});
 		})
 	});
+
+	describe('/:permalink/analysis', function () {
+		beforeEach(function () {
+			return models.image.create({
+				permalink: 'testPermalink'
+			});
+		});
+
+		it('should return error if permalink doesnt exist', function (done) {
+			testServer
+			.post('/api/images/wrong/analysis')
+			.expect(400, done);
+		});
+
+		it('should return error if no valid params found', function (done) {
+			testServer
+			.post('/api/images/testPermalink/analysis')
+			.send({wrong: 'nope', lolo: 'kekek'})
+			.expect(400, done);			
+		});
+
+		it('should enqueue job if valid params', function (done) {
+			testServer
+			.post('/api/images/testPermalink/analysis')
+			.send({ela: true, hsv: {whitebg: true}})
+			.end(function (err, res) {
+				expect(res.status).to.equal(200);
+				expect(res.body).to.have.property('jobId');
+
+				done();
+			});
+		});
+	});
 });
