f49d0f9e648d51de3a68457db2c3c3f0331a07c7
ebemunk <ebemunk@gmail.com>
2015-08-13 20:28:14 -0700

    queue + mock

---
+++ server/queue.js
@@ -0,0 +1,28 @@
+var siteConfig = require('./config.json');
+
+var monq = require('monq');
+var monqConn;
+
+//this is kinda sad but i cant find any other way to mock it in tests
+if( process.env.NODE_ENV !== 'test' ) {
+	monqConn = {
+		queue: function () {
+			return {
+				enqueue: function (queue, params, callback) {
+					return callback(null, {
+						data: {
+							_id: Math.random(),
+							params: params
+						}
+					});
+				}
+			}
+		}
+	};
+} else {
+	monqConn = monq(siteConfig.dbString);
+}
+
+var queue = monqConn.queue('phoenix');
+
+module.exports = queue;
