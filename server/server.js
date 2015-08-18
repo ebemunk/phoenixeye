71297785217213c2c55c04c10e20fc2359a269c0
ebemunk <ebemunk@gmail.com>
2015-08-18 03:09:51 -0700

    enable analyses route

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.get('/api', function (req, res) {
 
 //routes
 app.use('/api/images', require('./routes/images.js'));
-// app.use('/api/analyses', require('./routes/analyses.js'));
+app.use('/api/analyses', require('./routes/analyses.js'));
 // app.use('/api/jobs', require('./routes/jobs.js'));
 
 //static files
