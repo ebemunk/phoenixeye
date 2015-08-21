1593cc3cac4e846d86c1b9a648acd952cccef605
ebemunk <ebemunk@gmail.com>
2015-08-21 07:27:53 -0700

    jobs route

---
+++ server/routes/jobs.js
@@ -0,0 +1,29 @@
+var debug = require('debug')('server:routes:jobs');
+var config = require('../config.json');
+
+var router = require('express').Router();
+
+var HTTPError = require('node-http-error');
+
+//get job
+router.get('/:jobId', function (req, res, next) {
+	debug('/jobs/:jobId');
+
+	req.app.models.job.findOne({
+		id: req.params.jobId
+	})
+	.then(function (job) {
+		if( ! job ) {
+			throw new HTTPError(404, 'no job found with this id');
+		}
+
+		return res.json({
+			job: job
+		});
+	})
+	.catch(function (err) {
+		return next(err);
+	});
+});
+
+module.exports = router;
