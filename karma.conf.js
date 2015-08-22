46c16c3a8d9e188c967d6bd73d026419859392a6
ebemunk <ebemunk@gmail.com>
2015-08-22 00:25:04 -0700

    karma mocha reporter

--- karma.conf.js
+++ karma.conf.js
@@ -38,15 +38,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
-
-
-    preprocessors: {
-      // source files, that you wanna generate coverage for
-      // do not include tests or libraries
-      // (these files will be instrumented by Istanbul)
-      'client/js/**/*.js': ['coverage']
-    },
+    reporters: ['mocha', 'coverage'],
 
 
     coverageReporter: {
