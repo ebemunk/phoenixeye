d7d02c530baffe743bae59f0e88573197e12440f
ebemunk <ebemunk@gmail.com>
2015-09-22 22:13:09 -0700

    split up link function for slider directive

--- client/directives/slider.directive.js
+++ client/directives/slider.directive.js
@@ -8,7 +8,7 @@ slider.$inject = [
 ];
 
 function slider (noUiSlider) {
-	return {
+	var directive = {
 		restrict: 'E',
 		scope: {
 			//noUiSlider params
@@ -34,103 +34,107 @@ function slider (noUiSlider) {
 		require: 'ngModel',
 		template: '<div></div>',
 		replace: true,
-		link: function (scope, element, attrs, ngModelCtrl) {
-			var ngModelWatchDeregisterFn;
+		link: link
+	};
 
-			//prevent memory leaks on destroy
-			scope.$on('$destroy', function () {
+	return directive;
+
+	function link (scope, element, attrs, ngModelCtrl) {
+		var ngModelWatchDeregisterFn;
+
+		//prevent memory leaks on destroy
+		scope.$on('$destroy', function () {
+			destroy();
+		});
+
+		//watch noUiSlider options & reconstruct if they change
+		scope.$watchCollection(
+			function () {
+				return [
+					scope.step,
+					scope.start,
+					scope.connect,
+					scope.direction,
+					scope.snap,
+					scope.animate,
+					scope.range,
+					scope.orientation,
+					scope.margin,
+					scope.limit,
+					scope.behaviour,
+					scope.format,
+					scope.pips,
+					scope.formatInteger
+				];
+			},
+			function () {
+				initialize();
+			}
+		);
+
+		//initialize slider
+		function initialize () {
+			//convenience to get integer formatting
+			if( ! scope.format && attrs.hasOwnProperty('formatInteger') && attrs.formatInteger ) {
+				scope.format = {
+					to: function (val) {
+						return Math.round(val);
+					},
+					from: function (val) {
+						return val;
+					}
+				};
+			}
+
+			//remove old noUiSlider if it was already initialized
+			if( element[0].noUiSlider ) {
 				destroy();
+			}
+
+			//create element
+			var slider = noUiSlider.create(element[0], {
+				step: scope.step,
+				start: scope.start,
+				connect: scope.connect,
+				direction: scope.direction,
+				snap: scope.snap,
+				animate: scope.animate,
+				range: scope.range,
+				orientation: scope.orientation,
+				margin: scope.margin,
+				limit: scope.limit,
+				behaviour: scope.behaviour,
+				format: scope.format,
+				pips: scope.pips
 			});
 
-			//watch noUiSlider options & reconstruct if they change
-			scope.$watchCollection(
-				function () {
-					return [
-						scope.step,
-						scope.start,
-						scope.connect,
-						scope.direction,
-						scope.snap,
-						scope.animate,
-						scope.range,
-						scope.orientation,
-						scope.margin,
-						scope.limit,
-						scope.behaviour,
-						scope.format,
-						scope.pips,
-						scope.formatInteger
-					];
-				},
-				function () {
-					initialize();
+			scope.slider = slider;
+
+			var update = true;
+
+			element[0].noUiSlider.on('update', function () {
+				var val = element[0].noUiSlider.get();
+				update = false;
+				ngModelCtrl.$setViewValue(val);
+			});
+
+			ngModelWatchDeregisterFn = scope.$watch('ngModel', function (newV) {
+				if( ! update ) {
+					update = true;
+					return;
 				}
-			);
-
-			//initialize slider
-			function initialize () {
-				//convenience to get integer formatting
-				if( ! scope.format && attrs.hasOwnProperty('formatInteger') && attrs.formatInteger ) {
-					scope.format = {
-						to: function (val) {
-							return Math.round(val);
-						},
-						from: function (val) {
-							return val;
-						}
-					};
-				}
-
-				//remove old noUiSlider if it was already initialized
-				if( element[0].noUiSlider ) {
-					destroy();
-				}
-
-				//create element
-				var slider = noUiSlider.create(element[0], {
-					step: scope.step,
-					start: scope.start,
-					connect: scope.connect,
-					direction: scope.direction,
-					snap: scope.snap,
-					animate: scope.animate,
-					range: scope.range,
-					orientation: scope.orientation,
-					margin: scope.margin,
-					limit: scope.limit,
-					behaviour: scope.behaviour,
-					format: scope.format,
-					pips: scope.pips
-				});
-
-				scope.slider = slider;
-
-				var update = true;
-
-				element[0].noUiSlider.on('update', function () {
-					var val = element[0].noUiSlider.get();
-					update = false;
-					ngModelCtrl.$setViewValue(val);
-				});
-
-				ngModelWatchDeregisterFn = scope.$watch('ngModel', function (newV) {
-					if( ! update ) {
-						update = true;
-						return;
-					}
-					element[0].noUiSlider.set(newV);
-				});
-			}
-
-			//destroy slider and remove watchers/events
-			function destroy () {
-				if( angular.isFunction(ngModelWatchDeregisterFn) ) {
-					ngModelWatchDeregisterFn();
-				}
-
-				element[0].noUiSlider.off('update');
-				element[0].noUiSlider.destroy();
-			}
+				element[0].noUiSlider.set(newV);
+			});
 		}
-	};
+
+		//destroy slider and remove watchers/events
+		function destroy () {
+			if( angular.isFunction(ngModelWatchDeregisterFn) ) {
+				ngModelWatchDeregisterFn();
+			}
+
+			element[0].noUiSlider.off('update');
+			element[0].noUiSlider.destroy();
+		}
+	}
 }
