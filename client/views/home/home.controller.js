7ff0ddfd1095785e48c98503e81af4620a271e22
ebemunk <ebemunk@gmail.com>
2015-09-27 05:11:21 -0700

    submission policy agreement modal

--- client/views/home/home.controller.js
+++ client/views/home/home.controller.js
@@ -6,12 +6,16 @@ angular.module('phoenixeye')
 HomeController.$inject = [
 	'debug',
 	'$http',
+	'$q',
+	'$rootScope',
 	'$state',
 	'Upload',
-	'ngToast'
+	'ngToast',
+	'localStorageService',
+	'$modal'
 ];
 
-function HomeController (debug, $http, $state, Upload, ngToast) {
+function HomeController (debug, $http, $q, $rootScope, $state, Upload, ngToast, localStorageService, $modal) {
 	debug = debug('app:HomeController');
 
 	var vm = this;
@@ -21,53 +25,106 @@ function HomeController (debug, $http, $state, Upload, ngToast) {
 
 	function upload (file) {
 		if( file && file.length ) {
-			Upload.upload({
-				url: '/api/images/upload',
-				file: file
-			})
-			.progress(function (event) {
-				debug('progress', event);
-			})
-			.success(function (response, status) {
-				debug('success', response, status);
+			acceptedSubmissionPolicy()
+			.then(function () {
+				Upload.upload({
+					url: '/api/images/upload',
+					file: file
+				})
+				.progress(function (event) {
+					debug('progress', event);
+				})
+				.success(function (response, status) {
+					debug('success', response, status);
 
-				var jobId = response.jobId || null;
+					var jobId = response.jobId || null;
 
-				ngToast.success('Upload complete!');
+					ngToast.success('Upload complete!');
 
-				$state.go('image', {
-					image: response.image,
-					jobId: jobId,
-					permalink: response.image.permalink
+					$state.go('image', {
+						image: response.image,
+						jobId: jobId,
+						permalink: response.image.permalink
+					});
+				})
+				.catch(function (response) {
+					debug('error', response);
+					ngToast.danger('Something went wrong :(<br><strong>' + response.data.error + '</strong>');
 				});
 			})
-			.catch(function (response) {
-				debug('error', response);
-				ngToast.danger('Something went wrong :(<br><strong>' + response.data.error + '</strong>');
-			});
+			.catch(mustAcceptSubmissionPolicy);
 		}
 	}
 
 	function submit (url) {
-		$http({
-			method: 'POST',
-			url: 'api/images/submit',
-			data: {url: url}
-		})
-		.then(function (response, status) {
-			debug('success', response, status);
+		acceptedSubmissionPolicy()
+		.then(function () {
+			$http({
+				method: 'POST',
+				url: 'api/images/submit',
+				data: {url: url}
+			})
+			.then(function (response, status) {
+				debug('success', response, status);
 
-			var jobId = response.data.jobId || null;
+				var jobId = response.data.jobId || null;
 
-			$state.go('image', {
-				image: response.data.image,
-				jobId: jobId,
-				permalink: response.data.image.permalink
+				$state.go('image', {
+					image: response.data.image,
+					jobId: jobId,
+					permalink: response.data.image.permalink
+				});
+			})
+			.catch(function (response, status) {
+				debug('error', response, status);
+				ngToast.danger('Something went wrong :(<br><strong>' + response.data.error + '</strong>');
 			});
 		})
-		.catch(function (response, status) {
-			debug('error', response, status);
-			ngToast.danger('Something went wrong :(<br><strong>' + response.data.error + '</strong>');
-		});
+		.catch(mustAcceptSubmissionPolicy);
+	}
+
+	function acceptedSubmissionPolicy () {
+		var accepted = $q.defer();
+
+		if( localStorageService.get('acceptedSubmissionPolicy') ) {
+			accepted.resolve(true);
+		} else {
+			var modalScope = $rootScope.$new();
+			modalScope.ok = function () {
+				modal.close(true);
+			};
+
+			var modal = $modal.open({
+				animation: true,
+				templateUrl: 'components/imagePolicy/imagePolicy.html',
+				scope: modalScope,
+				backdrop: 'static',
+				keyboard: false
+			});
+
+			modal.result
+			.then(function (response) {
+				debug('modal response', response);
+
+				localStorageService.set('acceptedSubmissionPolicy', response);
+
+				if( ! response ) {
+					accepted.reject();
+					return;
+				}
+
+				accepted.resolve(true);
+			})
+			.catch(function (err) {
+				debug('modal fail', err);
+				accepted.reject();
+			});
+		}
+
+		return accepted.promise;
+	}
+
+	function mustAcceptSubmissionPolicy () {
+		ngToast.danger('You must accept the upload policy to submit images for analysis.');
 	}
 }
